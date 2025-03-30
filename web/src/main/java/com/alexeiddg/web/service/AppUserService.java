6e3ae22548bec3029f86501a9c9453952ef60a1c
alexeiddg <117191717+alexeiddg@users.noreply.github.com>
2025-03-29 23:57:40 -0600

    Add Base Telegram Springboot app

---
+++ web/src/main/java/com/alexeiddg/web/service/AppUserService.java
@@ -0,0 +1,13 @@
+package com.alexeiddg.web.service;
+
+import com.alexeiddg.web.repository.AppUserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserService {
+    private final AppUserRepository appUserRepository;
+
+    public AppUserService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+}
