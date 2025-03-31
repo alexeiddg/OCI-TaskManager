1035d028cc879ffe52a8c540858de78e80a051a6
alexeiddg <117191717+alexeiddg@users.noreply.github.com>
2025-03-31 16:33:31 -0600

    Ability Bot Config

--- telegram/src/main/java/com/alexeiddg/telegram/bot/TaskManagerBot.java
+++ telegram/src/main/java/com/alexeiddg/telegram/bot/TaskManagerBot.java
@@ -1,67 +1,55 @@
 package com.alexeiddg.telegram.bot;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
-import org.telegram.telegrambots.longpolling.BotSession;
-import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
-import org.telegram.telegrambots.longpolling.starter.AfterBotRegistration;
-import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
-import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.TelegramClient;
+import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.Locality;
+import org.telegram.abilitybots.api.objects.Privacy;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 /**
- * This class is the main class of the bot. It is annotated with @Component to be instantiated by Spring.
- * The Class read the bot token from the application.properties file and implements the SpringLongPollingBot interface.
+ * This class handles creating the bot and registering it with the Telegram API.
+ * The @PostConstruct annotation is used to register the bot when the application starts.
+ * Spring will not create the bean by itself, hence the need for the @PostConstruct annotation.
  */
 
+@Slf4j
 @Component
-public class TaskManagerBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
-    private final TelegramClient telegramClient;
-    private final String botToken;
-
-    public TaskManagerBot(@Value("${telegram.bot.token}") String botToken) {
-        this.botToken = botToken;
-        telegramClient = new OkHttpTelegramClient(botToken);
+public class TaskManagerBot extends AbilityBot {
+    public TaskManagerBot(
+            @Value("${telegram.bot.username}") String botUsername,
+            @Value("${telegram.bot.token}") String botToken)
+    {
+        super(botToken, botUsername);
     }
 
-
-    @Override
-    public String getBotToken() {
-        return botToken;
-    }
-
-    @Override
-    public LongPollingUpdateConsumer getUpdatesConsumer() {
-        return this;
-    }
-
-    @Override
-    public void consume(Update update) {
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            // Set variables
-            String message_text = update.getMessage().getText();
-            long chat_id = update.getMessage().getChatId();
-
-            SendMessage message = SendMessage // Create a message object
-                    .builder()
-                    .chatId(chat_id)
-                    .text(message_text)
-                    .build();
-            try {
-                telegramClient.execute(message); // Sending our message object to user
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+    @PostConstruct
+    public void registerBot() {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(this);
+        } catch (Exception e) {
+            TaskManagerBot.log.error("Error while registering bot", e);
         }
     }
 
-    @AfterBotRegistration
-    public void afterRegistration(BotSession botSession) {
-        System.out.println("Registered bot running state is: " + botSession.isRunning());
+    @Override
+    public long creatorId() {
+        return 1L;
+    }
+
+    public Ability start() {
+        return Ability
+                .builder()
+                .name("start")
+                .info("Start command")
+                .locality(Locality.ALL)
+                .privacy(Privacy.PUBLIC)
+                .action(ctx -> silent.send("Hello, welcome to the bot!", ctx.chatId()))
+                .build();
     }
 }
