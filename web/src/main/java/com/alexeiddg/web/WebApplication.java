4785f07311065ff65022342405518c9d0de4cfa9
Iker Casillas <ikercasillasp2@gmail.com>
2025-04-03 01:01:02 -0600

    Fix package paths

--- web/src/main/java/com/alexeiddg/web/WebApplication.java
+++ web/src/main/java/com/alexeiddg/web/WebApplication.java
@@ -3,9 +3,12 @@ package com.alexeiddg.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "model")
+@EntityScan(basePackages = "com.alexeiddg.web.model")
+@EnableJpaRepositories(basePackages = "com.alexeiddg.web.repository")
+
 public class WebApplication {
 
 	public static void main(String[] args) {
