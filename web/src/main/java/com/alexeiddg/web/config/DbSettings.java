543908960914ff99d621546b95a3f4d5f3d2559c
alexeiddg <117191717+alexeiddg@users.noreply.github.com>
2025-04-25 11:25:41 -0600

    update

--- web/src/main/java/com/alexeiddg/web/config/DbSettings.java
+++ web/src/main/java/com/alexeiddg/web/config/DbSettings.java
@@ -1,11 +1,11 @@
 // package com.alexeiddg.web.config;
-
-
+//
+//
 // import lombok.Getter;
 // import lombok.Setter;
 // import org.springframework.boot.context.properties.ConfigurationProperties;
 // import org.springframework.context.annotation.Configuration;
-
+//
 // @Setter
 //@Getter
 //@Configuration
