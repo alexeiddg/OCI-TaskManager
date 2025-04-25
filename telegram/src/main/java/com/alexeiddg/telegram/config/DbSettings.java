d76d29cccd6f2ce11ea6015c4f6ba30886ede2bc
alexeiddg <117191717+alexeiddg@users.noreply.github.com>
2025-04-25 09:58:31 -0600

    update

--- telegram/src/main/java/com/alexeiddg/telegram/config/DbSettings.java
+++ telegram/src/main/java/com/alexeiddg/telegram/config/DbSettings.java
@@ -1,18 +1,18 @@
-package com.alexeiddg.telegram.config;
-
-
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Setter
-@Getter
-@Configuration
-@ConfigurationProperties(prefix = "spring.datasource")
-public class DbSettings {
-    private String url;
-    private String username;
-    private String password;
-    private String driver_class_name;
-}
+//package com.alexeiddg.telegram.config;
+//
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Setter
+//@Getter
+//@Configuration
+//@ConfigurationProperties(prefix = "spring.datasource")
+//public class DbSettings {
+//    private String url;
+//    private String username;
+//    private String password;
+//    private String driver_class_name;
+//}
--- web/src/main/java/com/alexeiddg/web/config/DbSettings.java
+++ web/src/main/java/com/alexeiddg/web/config/DbSettings.java
@@ -1,18 +1,18 @@
-package com.alexeiddg.web.config;
+// package com.alexeiddg.web.config;
 
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+// import lombok.Getter;
+// import lombok.Setter;
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.context.annotation.Configuration;
 
-@Setter
-@Getter
-@Configuration
-@ConfigurationProperties(prefix = "spring.datasource")
-public class DbSettings {
-    private String url;
-    private String username;
-    private String password;
-    private String driver_class_name;
-}
+// @Setter
+//@Getter
+//@Configuration
+//@ConfigurationProperties(prefix = "spring.datasource")
+//public class DbSettings {
+//    private String url;
+//    private String username;
+//    private String password;
+//    private String driver_class_name;
+//}
